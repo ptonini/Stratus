63bf4a3a07c5cbd0c1dd803031f0822d64370a3d
ptonini <pedro.tonini@hotmail.com>
2015-03-10 19:19:16 -0300

    Create stratus.py

---
+++ stratus.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+from gmusicapi import Musicmanager
+from gmusicapi import Mobileclient
+from tabulate import tabulate
+from prettytable import PrettyTable
+from prettytable import PLAIN_COLUMNS, FRAME, ALL, NONE
+#mm = Musicmanager()
+#mm.login(oauth_credentials='/home/ptonini/oauth.cred')
+
+api = Mobileclient()
+logged_in = api.login('pedro.tonini', 'besTeira07')
+
+songlist = api.get_all_songs()
+
+t = PrettyTable(['Key', 'Value'])
+t.hrules = ALL
+t.header_style = 'upper'
+t.align = 'l'
+t.padding_width = 3
+song = songlist[0]
+for k, v in song.iteritems():
+    t.add_row([k, v])
+print t.get_string(sortby='Key')
+print len(songlist)
