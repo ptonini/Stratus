2ffc4a4c29a6b404a5b7bf184f447a9fd4c7ac55
ptonini <kakaroto00>
2015-03-20 19:31:35 -0300

    commit now

--- stratus.py
+++ stratus.py
@@ -5,21 +5,40 @@ from gmusicapi import Mobileclient
 from tabulate import tabulate
 from prettytable import PrettyTable
 from prettytable import PLAIN_COLUMNS, FRAME, ALL, NONE
-#mm = Musicmanager()
-#mm.login(oauth_credentials='/home/ptonini/oauth.cred')
 
-api = Mobileclient()
-logged_in = api.login('', '')
+import mutagen
+import eyeD3
 
-songlist = api.get_all_songs()
+import os
 
-t = PrettyTable(['Key', 'Value'])
-t.hrules = ALL
-t.header_style = 'upper'
-t.align = 'l'
-t.padding_width = 3
-song = songlist[0]
-for k, v in song.iteritems():
-    t.add_row([k, v])
-print t.get_string(sortby='Key')
-print len(songlist)
+
+def teste ():
+
+    #mm = Musicmanager()
+    #mm.login(oauth_credentials='/home/ptonini/oauth.cred')
+
+    api = Mobileclient()
+    logged_in = api.login('', '')
+
+    songlist = api.get_all_songs()
+
+    t = PrettyTable(['Key', 'Value'])
+    t.hrules = ALL
+    t.header_style = 'upper'
+    t.align = 'l'
+    t.padding_width = 3
+    song = songlist[0]
+    for k, v in song.iteritems():
+        t.add_row([k, v])
+    print t.get_string(sortby='Key')
+    print len(songlist)
+
+
+
+for root, dirs, files in os.walk('/mnt/Musicas/Google Music', topdown=False):
+    for name in files:
+        print(os.path.join(root, name))
+
+
+
+
