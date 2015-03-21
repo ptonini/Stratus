bbbadd4640ab5710a8d192cc47fa21ab804de4d4
ptonini <kakaroto00>
2015-03-21 19:52:18 -0300

    commit now

--- stratus.py
+++ stratus.py
@@ -1,64 +1,75 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 
 import re
 import os
 
 from gmusicapi import Mobileclient
-from mutagen.id3 import ID3
+from mutagen.easyid3 import EasyID3
 from pymongo import MongoClient
+
 
 class Tracks:
     """"""
 
-    def __init__(self, file):
+    def __init__(self, file, songlist):
         """ """
 
-        tag = ID3(file)
+        tags = EasyID3(file)
         self.filename = file
-        self.genre = str(tag['TCON'].pprint())[5:]
-        self.artist = str(tag['TPE1'].pprint())[5:]
-        self.albumArtist = str(tag['TPE2'].pprint())[5:]
-        self.album = str(tag['TALB'].pprint())[5:]
-        self.year = int(str(tag['TDRC'].pprint())[5:])
-        self.trackNumber = int(str(tag['TRCK'].pprint())[5:])
-        self.title = str(tag['TIT2'].pprint())[5:]
-
-
-    def matchSong(self, songlist):
-        """"""
-        matched = False
+        self.genre = tags['genre'][0]
+        self.artist = tags['artist'][0]
+        self.albumArtist = tags['performer'][0]
+        self.album = tags['album'][0]
+        self.year = tags['date'][0]
+        self.trackNumber = tags['tracknumber'][0]
+        self.title = tags['title'][0]
         for song in songlist:
             if song['title'] == self.title and song['albumArtist'] == self.albumArtist and song['album'] == self.album \
                     and song['trackNumber'] == self.trackNumber:
-                print self.title, '- is on Google Music'
                 self.gmusic_id = song['id']
-                matched = True
                 break
-        if not matched:
-            pass
-            print '!!! Could not find "' + self.title + '" on Google Music'
 
 
-def getSongList(user, password):
+
+def getSonglist(user, password):
     api = Mobileclient()
     api.login(user, password)
     return api.get_all_songs()
 
 
-songlist = getSongList('', '')
-client = MongoClient('mongodb://localhost:27017/')
-db = client.stratus
-trackColl = db.tracks
-
-for root, path, files in os.walk('/mnt/Musicas/01 Principal/Albums/AC DC/'):
-    for name in files:
-        p = re.compile('.*.mp3$')
-        if p.match(name):
-            file = os.path.join(root, name)
-            track = Tracks(file)
-            if trackColl.find({"filename": file}).count() == 0:
-                trackColl.insert(track.__dict__)
-            elif trackColl.find({"filename": file}).count() > 1:
-                print 'Error: duplicate tracks on database'
+def buildTrackCollection(filelist, songlist, tracksColl):
+    for file in filelist:
+        track = Tracks(file, songlist)
+        trackCount = tracksColl.find({"filename": file}).count()
+        if trackCount == 0:
+            tracksColl.insert(track.__dict__)
+        elif trackCount > 1:
+            print 'Error: duplicate tracks on database'
 
 
+def openTracksCollection(database):
+    client = MongoClient()
+    db = client.stratus
+    tracksColl = db.tracks
+    return tracksColl
+
+def getFileList(folder):
+    filelist = []
+    for root, path, files in os.walk(folder):
+        for name in files:
+            p = re.compile('.*.mp3$')
+            if p.match(name):
+                file = os.path.join(root, name)
+                filelist.append(file)
+    return filelist
+
+def main():
+    songlist = getSonglist()
+    tracksColl = openTracksCollection('mongodb://localhost:27017/')
+    filelist = getFileList('/mnt/Musicas/Google Music')
+    buildTrackCollection(filelist, songlist, tracksColl)
+
+if __name__ == '__main__':
+    main()
